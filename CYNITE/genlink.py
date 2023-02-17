c27b9ff1eb7573b5a99c6d7d3ace5cbe54fc4d9f
Psycho Botz <124176508+PSYCHO-BOTZ@users.noreply.github.com>
2023-02-17 16:28:47 +0530

    Update genlink.py

--- CYNITE/genlink.py
+++ CYNITE/genlink.py
@@ -40,10 +40,10 @@ async def gen_link_s(bot, message):
 @Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
 async def gen_link_batch(bot, message):
     if " " not in message.text:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/cyniteBackup/3 https://t.me/CyniteBackup/9</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/Team_netflix/3 https://t.me/CyniteBackup/9</code>.")
     links = message.text.strip().split(" ")
     if len(links) != 3:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/CyniteBackup/3 https://t.me/CyniteBackup/9</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/Team_netflix/3 https://t.me/CyniteBackup/9</code>.")
     cmd, first, last = links
     regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
     match = regex.match(first)
